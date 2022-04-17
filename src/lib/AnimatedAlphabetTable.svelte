5ee3502bf5b3a3a371868e6cd9b8fbf831daaf12
frederickobrien <frederik.obrien@gmail.com>
2022-04-17 18:30:57 +0100

    Data structure for outlines
    Converted alphabet into data structure with which SVGs can be built. Used this to generate a new, animated alphabet grid which shows the outlines being traced when users hover over.

---
+++ src/lib/AnimatedAlphabetTable.svelte
@@ -0,0 +1,79 @@
+<script>
+	import { alphabet } from '../data/outlines/alphabet';
+</script>
+
+<div class="animated-container">
+	{#each alphabet as letter}
+		<div class="outline-container">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				width="100pt"
+				height="100pt"
+				viewBox="0 0 720 720"
+			>
+				<defs />
+				<path
+					id="shape0"
+					transform="translate(0, 458.461538461538)"
+					fill="#D3D3D3"
+					fill-rule="evenodd"
+					stroke="#D3D3D3"
+					stroke-width="5"
+					stroke-linecap="square"
+					stroke-linejoin="miter"
+					stroke-miterlimit="2"
+					d="M 159 1 C 279.658 0.6508 522.393 0.3254 493 1"
+				/>
+				{#each letter.lines as line}
+					<path
+						class="path"
+						transform="translate({line.translateValues})"
+						fill="none"
+						stroke="#000000"
+						stroke-width="13"
+						stroke-linecap="round"
+						stroke-linejoin="bevel"
+						d={line.path}
+					/>
+				{/each}
+			</svg>
+			<div class="outline-label">{letter.name}</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.animated-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 70px;
+	}
+
+	.outline-container {
+		padding: 0 20px 40px 20px;
+		border-radius: 15px;
+		box-shadow: 0px 0px 10px 3px #e1e1e1;
+		border-radius: 10px;
+	}
+
+	.outline-container:hover .path {
+		stroke-dasharray: 1000;
+		stroke-dashoffset: 1000;
+		animation: dash 2s linear infinite;
+	}
+
+	.outline-label {
+		text-align: center;
+	}
+
+	@keyframes dash {
+		from {
+			stroke-dashoffset: 1100;
+		}
+		to {
+			stroke-dashoffset: 0;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,18 @@
 <script>
-	import B from '$lib/animated-letters/B.svelte';
-	import F from '$lib/animated-letters/F.svelte';
-
-	import AlphabetTable from '../lib/AlphabetTable.svelte';
+	import StaticAlphabetTable from '../lib/StaticAlphabetTable.svelte';
+	import AnimatedAlphabetTable from '../lib/AnimatedAlphabetTable.svelte';
 </script>
 
-<main>
-	<AlphabetTable />
+<svelte:head>
+	<title>Teeline Online</title>
+</svelte:head>
 
-	<div>
-		<h3>And here are a couple of animated outlines - 'b' and 'f'</h3>
-		<B />
-		<F />
-	</div>
+<main>
+	<AnimatedAlphabetTable />
 </main>
 
 <style>
 	main {
 		margin: 50px 100px;
-	}
-
-	h3 {
-		padding-top: 100px;
 	}
 </style>
