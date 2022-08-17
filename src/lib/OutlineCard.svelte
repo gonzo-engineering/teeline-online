1f4f86200aaccce8b558cd499fe62a9b7d32dabf
frederickobrien <frederik.obrien@gmail.com>
2022-08-17 23:48:22 +0100

    Card content container
    So that they look purty

--- src/lib/OutlineCard.svelte
+++ src/lib/OutlineCard.svelte
@@ -3,40 +3,42 @@
 </script>
 
 <div class="outline-container">
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		xmlns:xlink="http://www.w3.org/1999/xlink"
-		width="100%"
-		viewBox="0 0 750 750"
-	>
-		<defs />
-		<path
-			id="shape0"
-			transform="translate(0, 458.461538461538)"
-			fill="#D3D3D3"
-			fill-rule="evenodd"
-			stroke="#D3D3D3"
-			stroke-width="5"
-			stroke-linecap="square"
-			stroke-linejoin="miter"
-			stroke-miterlimit="2"
-			d="M 80 1 C 279.658 0.6508 522.393 0.3254 650 1"
-		/>
-		{#each outline.lines as line, i}
+	<div class="outline-content">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			width="100%"
+			viewBox="0 0 750 750"
+		>
+			<defs />
 			<path
-				class="path"
-				transform="translate({line.translateValues})"
-				fill="none"
-				stroke="#000000"
-				stroke-width="10"
-				stroke-linecap="round"
-				stroke-linejoin="bevel"
-				style="animation-delay: {i}s"
-				d={line.path}
+				id="shape0"
+				transform="translate(0, 458.461538461538)"
+				fill="#D3D3D3"
+				fill-rule="evenodd"
+				stroke="#D3D3D3"
+				stroke-width="5"
+				stroke-linecap="square"
+				stroke-linejoin="miter"
+				stroke-miterlimit="2"
+				d="M 80 1 C 279.658 0.6508 522.393 0.3254 650 1"
 			/>
-		{/each}
-	</svg>
-	<div class="outline-label">{outline.name.toString().replace(/,/g, ' / ')}</div>
+			{#each outline.lines as line, i}
+				<path
+					class="path"
+					transform="translate({line.translateValues})"
+					fill="none"
+					stroke="#000000"
+					stroke-width="10"
+					stroke-linecap="round"
+					stroke-linejoin="bevel"
+					style="animation-delay: {i}s"
+					d={line.path}
+				/>
+			{/each}
+		</svg>
+		<div class="outline-label">{outline.name.toString().replace(/,/g, ' / ')}</div>
+	</div>
 </div>
 
 <style>
@@ -57,6 +59,10 @@
 	.outline-label {
 		text-align: center;
 		font-size: 1.9rem;
+	}
+
+	.outline-content {
+		padding: 20px;
 	}
 
 	@keyframes dash {
