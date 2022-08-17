343de441d6f62a56f3b7369a0ce64bd0b4b700e8
frederickobrien <frederik.obrien@gmail.com>
2022-08-17 23:31:51 +0100

    SVG converter script can now handle multi-pathed outlines

--- scripts/turnSvgsIntoOutlineObjects.js
+++ scripts/turnSvgsIntoOutlineObjects.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import { parse } from 'svg-parser';
+import { readdirSync, readFileSync, writeFile } from 'fs';
 
 const svgFolderPath = '../static/outlines/shortcuts/';
 
@@ -9,41 +8,40 @@ function roundToNearest10(num) {
     return Math.round(num / 10) * 10;
 }
 
-fs.readdirSync(svgFolderPath).forEach(file => {
+readdirSync(svgFolderPath).forEach(file => {
 
 
-    const fileContent = fs.readFileSync(svgFolderPath + file, { encoding: "utf8" })
+    const fileContent = readFileSync(svgFolderPath + file, { encoding: "utf8" })
     const lineCount = (fileContent.match(/translate/g) || []).length
 
-    console.log(`SVG being parsed: ${file}`);
-    console.log(`Lines in file: ${lineCount}`)
-
-    function inferLinePath(stringifiedSvg) {
-        const linePath = stringifiedSvg.split(' d="')[1].split('"/>')[0];
+    function inferLinePath(stringifiedSvg, lineNumber) {
+        const linePath = stringifiedSvg.split(' d="')[lineNumber].split('"/>')[0];
         return linePath;
     }
 
-    function inferTranslateValues(stringifiedSvg) {
-        const translateValuesAsStrings = stringifiedSvg.split('translate(')[1].split(')"')[0];
+    function inferTranslateValues(stringifiedSvg, lineNumber) {
+        const translateValuesAsStrings = stringifiedSvg.split('translate(')[lineNumber].split(')"')[0];
         const [x, y] = translateValuesAsStrings.split(',');
         return [roundToNearest10(+x), roundToNearest10(+y)]
     }
 
-    if (lineCount === 1) {
-        outlinesObjectArray.push({
-            name: file.replace('.svg', ''),
-            lines: [
-                {
-                    path: inferLinePath(fileContent),
-                    translateValues: inferTranslateValues(fileContent)
-                }
-            ]
+    let outlineObject = {
+        name: [file.replace('.svg', '')],
+        lines: []
+    }
+
+    for (let i = 0; i < lineCount; i++) {
+        outlineObject.lines.push({
+            path: inferLinePath(fileContent, i + 1),
+            translateValues: inferTranslateValues(fileContent, i + 1)
         })
     }
 
+    outlinesObjectArray.push(outlineObject);
+
 })
 
-// console.log(JSON.stringify(outlinesObjectArray))
+console.log(JSON.stringify(outlinesObjectArray))
 
 // fs.writeFile('/test.json', JSON.stringify(outlinesObjectArray), err => {
 //     if (err) {
