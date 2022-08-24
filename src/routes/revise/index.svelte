3f585e6b8f4d9f583d0d94ba1f3f886ed59ed8f9
frederickobrien <frederik.obrien@gmail.com>
2022-08-25 00:37:45 +0100

    Update revision outline cards with button or spacebar

--- src/routes/revise/index.svelte
+++ src/routes/revise/index.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { validate_each_keys } from 'svelte/internal';
+
 	import { outlines } from '../../data/outlines/outlines';
 	import FlippingOutlineCard from '../../lib/cards/FlippingOutlineCard.svelte';
 
@@ -12,18 +14,40 @@
 	shuffleArray(outlines);
 
 	let outline = outlines[0];
+	let key;
+	let keyCode;
+
+	function getRandomArbitrary(min, max) {
+		return Math.ceil(Math.random() * (max - min) + min);
+	}
+
+	function changeOutline() {
+		let randomNumber = getRandomArbitrary(0, outlines.length - 1);
+		console.log(randomNumber);
+		outline = outlines[randomNumber];
+	}
+
+	function handleKeydown(event) {
+		if (event.keyCode === 32) {
+			changeOutline();
+		}
+	}
 </script>
 
 <svelte:head>
 	<title>Revise | teeline.online</title>
 </svelte:head>
 
+<svelte:window on:keydown={handleKeydown} />
+
 <div class="content">
-	<p>What does this outline stand for? Hover/tap to see the answer</p>
+	<p>Hover/tap to see what the outline stands for</p>
 
 	<FlippingOutlineCard {outline} />
 
-	<p>Refresh for a new card (smarter system coming soon)</p>
+	<div class="button-container">
+		<button class="button" on:click={changeOutline}>New outline</button>
+	</div>
 </div>
 
 <style>
@@ -33,5 +57,16 @@
 
 	p {
 		text-align: center;
+	}
+
+	.button-container {
+		text-align: center;
+	}
+
+	button {
+		font-size: 2rem;
+		/* box-shadow: 0px 0px 10px 1px #e1e1e1; */
+		border-radius: 10px;
+		padding: 10px 20px;
 	}
 </style>
