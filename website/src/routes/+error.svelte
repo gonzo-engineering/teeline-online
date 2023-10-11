2b627c69bdeb41907b378d83cf5c80fc5e72b969
Frederick O'Brien <frederik.obrien@gmail.com>
2023-10-11 23:02:04 +0100

    Add basic error page

---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Error | teeline.online</title>
+</svelte:head>
+
+<div class="error-content-container">
+	<h1>404</h1>
+	<p>
+		Oh dear. This page doesn't exist, and probably never did. Let's get you <a href="/">home</a> before
+		you hurt yourself.
+	</p>
+</div>
+
+<style>
+	.error-content-container {
+		text-align: center;
+		margin-top: 5rem;
+	}
+</style>
