8f3a897f8bd363f1349fd808ce2d2de521fe5c06
frederickobrien <frederik.obrien@gmail.com>
2024-01-25 16:23:07 +0000

    Words ending with 'ue' disemvoweler logic

--- website/src/scripts/disemvowel.ts
+++ website/src/scripts/disemvowel.ts
@@ -5,8 +5,10 @@ export const disemvowelWord = (word: string) => {
 	const firstLetter = word.charAt(0);
 	const finalLetter = word.charAt(word.length - 1);
 	const finalLetterIsSoundedVowel = ['a', 'i', 'o', 'u'].includes(finalLetter.toLowerCase());
+	const endsWithUe = word.slice(-2) === 'ue';
 	const wordNoVowelsOrDoubles = word
 		.replace('bb', 'b')
+		.replace('cc', 'c')
 		.replace('dd', 'd')
 		.replace('ff', 'f')
 		.replace('gg', 'g')
@@ -26,6 +28,10 @@ export const disemvowelWord = (word: string) => {
 		return firstLetter + wordNoVowelsOrDoubles + finalLetter;
 	else if (!isVowel(firstLetter) && finalLetterIsSoundedVowel)
 		return wordNoVowelsOrDoubles + finalLetter;
+	else if (isVowel(firstLetter) && endsWithUe)
+		return firstLetter + wordNoVowelsOrDoubles + 'u'
+	else if (!isVowel(firstLetter) && endsWithUe)
+		return wordNoVowelsOrDoubles + 'u';
 	else if (isVowel(firstLetter)) return firstLetter + wordNoVowelsOrDoubles;
 	else return wordNoVowelsOrDoubles;
 };
