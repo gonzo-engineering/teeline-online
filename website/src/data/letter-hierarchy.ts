156cbae9ae516ee9721dade28a9e13c94663ea33
Frederick O'Brien <frederik.obrien@gmail.com>
2024-02-08 15:47:33 +0000

    Move 'r' into dominant consonant group

--- website/src/data/letter-hierarchy.ts
+++ website/src/data/letter-hierarchy.ts
@@ -1,22 +1,7 @@
 export const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-export const dominantConsonants = ['b', 'd', 'h', 'j', 'l', 'p', 't'];
+export const dominantConsonants = ['b', 'd', 'h', 'j', 'l', 'p', 'r', 't'];
 
-export const passiveConsonants = [
-	'c',
-	'f',
-	'g',
-	'k',
-	'm',
-	'n',
-	'q',
-	'r',
-	's',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z'
-];
+export const passiveConsonants = ['c', 'f', 'g', 'k', 'm', 'n', 'q', 's', 'v', 'w', 'x', 'y', 'z'];
 
 export const alphabet = [...vowels, ...dominantConsonants, ...passiveConsonants].sort();
