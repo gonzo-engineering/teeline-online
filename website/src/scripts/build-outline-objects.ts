3635b4fb4959fd2bf8221730aed1d21ddd9869d1
Frederick O'Brien <frederik.obrien@gmail.com>
2024-03-02 15:50:00 +0000

    Refactor code
    With help from ChatGPT for shits and giggles

--- website/src/scripts/build-outline-objects.ts
+++ website/src/scripts/build-outline-objects.ts
@@ -31,30 +31,37 @@ export const findOrCreateOutlineObject = (
 	}
 
 	// Find matching letter or letter grouping at start of word until no more letters
-	const turnWordIntoOutlineObjects = (
+	function mapWordToOutlines(
 		word: string,
-		lettersAndGroupings: OutlineObject[],
+		outlines: OutlineObject[],
 		wordOutlines: OutlineObject[] = []
-	): OutlineObject[] => {
-		const multiLetterGroupingMatch = lettersAndGroupings.find((outline) =>
-			outline.letterGroupings.some((grouping) => word.startsWith(grouping) && grouping.length > 1)
-		);
-		let match = word.charAt(0);
-		lettersAndGroupings.forEach((outline) =>
-			outline.letterGroupings.forEach((grouping) => {
-				if (word.startsWith(grouping) && grouping.length > match.length) match = grouping;
-			})
-		);
-		const outlineObject = multiLetterGroupingMatch
-			? multiLetterGroupingMatch
-			: lettersAndGroupings.find((outline) => outline.letterGroupings.includes(match));
-		const updatedWordOutlines = wordOutlines.concat(outlineObject);
-		const updatedWord = word.slice(match.length);
-		if (updatedWord.length === 0) return updatedWordOutlines;
-		else return turnWordIntoOutlineObjects(updatedWord, lettersAndGroupings, updatedWordOutlines);
-	};
+	): OutlineObject[] {
+		if (!word) return wordOutlines; // Base case for recursion
 
-	const lettersObjectArray = turnWordIntoOutlineObjects(cleanedWord, outlines);
+		// Find the longest matching letter grouping at the start of the word
+		let longestMatch = '';
+		let matchedOutline: OutlineObject | undefined;
+
+		for (const outline of outlines) {
+			for (const grouping of outline.letterGroupings) {
+				if (word.startsWith(grouping) && grouping.length > longestMatch.length) {
+					longestMatch = grouping;
+					matchedOutline = outline;
+				}
+			}
+		}
+
+		// If no match was found, return the accumulated word outlines
+		if (!matchedOutline) return wordOutlines;
+
+		// Otherwise, append the found outline and continue recursively
+		return mapWordToOutlines(word.slice(longestMatch.length), outlines, [
+			...wordOutlines,
+			matchedOutline
+		]);
+	}
+
+	const lettersObjectArray = mapWordToOutlines(cleanedWord, outlines);
 
 	// we need to get the first starting point
 	const startingObject = createStartingObject(cleanedWord, lettersObjectArray);
@@ -100,28 +107,40 @@ export const findOrCreateOutlineObject = (
 	};
 };
 
-export const createOutlineObjects = (
+export function convertPassageToOutlines(
 	passage: string,
-	lettersAndGroupings: OutlineObject[],
+	outlines: OutlineObject[],
 	result: OutlineObject[] = []
-): OutlineObject[] => {
+): OutlineObject[] {
 	const cleanedPassage = passage.replace(punctuationRegex, '');
-	const multiWordSpecialMatch = lettersAndGroupings.find((outline) =>
-		outline.specialOutlineMeanings.some(
-			(meaning) => cleanedPassage.startsWith(meaning) && meaning.split(' ').length > 1
-		)
-	);
-	let match = cleanedPassage.split(' ')[0];
-	lettersAndGroupings.forEach((outline) => {
-		outline.specialOutlineMeanings.forEach((meaning) => {
-			if (passage.startsWith(meaning) && meaning.split(' ').length > 1) match = meaning;
-		});
-	});
-	const outlineObject = multiWordSpecialMatch
-		? multiWordSpecialMatch
-		: findOrCreateOutlineObject(match, lettersAndGroupings);
-	const updatedResult = result.concat(outlineObject);
-	const updatedPassage = cleanedPassage.slice(match.length).trim();
-	if (updatedPassage === '') return updatedResult;
-	else return createOutlineObjects(updatedPassage, lettersAndGroupings, updatedResult);
-};
+	let longestMatch = '';
+	let matchedOutline: OutlineObject | undefined;
+
+	// Find the longest special meaning match
+	for (const outline of outlines) {
+		for (const meaning of outline.specialOutlineMeanings) {
+			if (
+				cleanedPassage.startsWith(meaning) &&
+				meaning.split(' ').length > longestMatch.split(' ').length
+				// TODO: Check that the match is for complete words. 'And there are' should
+				// not be matching with the special outline 'and the'
+			) {
+				longestMatch = meaning;
+				matchedOutline = outline;
+			}
+		}
+	}
+
+	if (!matchedOutline) {
+		// Default to the first word if no special meaning found
+		longestMatch = cleanedPassage.split(' ')[0];
+		matchedOutline = findOrCreateOutlineObject(longestMatch, outlines);
+	}
+
+	const updatedResult = [...result, matchedOutline];
+	const updatedPassage = cleanedPassage.slice(longestMatch.length).trim();
+
+	return updatedPassage === ''
+		? updatedResult
+		: convertPassageToOutlines(updatedPassage, outlines, updatedResult);
+}
