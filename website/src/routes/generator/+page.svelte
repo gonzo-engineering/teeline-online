ed408577d58ee7d0052aebb56baba2c85a49f79c
Frederick O'Brien <frederik.obrien@gmail.com>
2024-03-04 20:57:18 +0000

    Update +page.svelte
    Addresses a bug flagged by a user. Might be worth standardising when the `toLowerCase` step happens but that's a problem for future Fred

--- website/src/routes/generator/+page.svelte
+++ website/src/routes/generator/+page.svelte
@@ -19,7 +19,7 @@
 		class="search-input search-input-generator"
 		placeholder="Generate outlines..."
 	/>
-	<ShorthandPassage text={inputText.trim()} />
+	<ShorthandPassage text={inputText.trim().toLowerCase()} />
 	<div style="width: 50%; margin: auto; text-align: center;">
 		<p>This feature is a work in progress so take results with a grain of salt.</p>
 	</div>
