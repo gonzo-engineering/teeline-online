0195e823b117fa587733e485b122527f0eeb11d1
frederickobrien <frederik.obrien@gmail.com>
2024-04-26 16:29:04 +0100

    Refactor line and outline length inference, add testing

---
+++ website/src/scripts/line-length-inference.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest';
+import { inferPrecedingLinesLength } from './line-length-inference';
+import { findOrCreateOutlineObject } from './build-outline-objects';
+import { hydratedData } from './hydrate-outline-data';
+
+test('testing preceding line length inference', () => {
+    expect(inferPrecedingLinesLength(findOrCreateOutlineObject("as a result", hydratedData), 2)).toBe(235);
+    expect(inferPrecedingLinesLength(findOrCreateOutlineObject("that the", hydratedData), 1)).toBe(428);
+});
---
+++ website/src/scripts/line-length-inference.ts
@@ -0,0 +1,19 @@
+import type { OutlineObject } from "../data/interfaces/interfaces";
+
+/**
+ * Typical distance between the end of the previous line and the start of the next
+ * We could calculate this precisely from the paths, but it’s probably too much effort.
+ */
+const pause = 60;
+
+export const inferPrecedingLinesLength = (outline: OutlineObject, index: number) =>
+    outline.lines
+        .slice(0, index)
+        .map(({ length }) => length)
+        .reduce((a, b) => a + b + pause, 0);
+
+export const inferPrecedingOutlinesLength = (outlineObjects: OutlineObject[], index: number) =>
+    outlineObjects
+        .slice(0, index)
+        .map(({ lines }) => lines.map(({ length }) => length).reduce((a, b) => a + b + pause, 0))
+        .reduce((a, b) => a + b, 0);
