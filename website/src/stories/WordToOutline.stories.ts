69b1a3fb188da77d75f1b9a49c3958c34cf4a1f7
Frederick O'Brien <frederik.obrien@gmail.com>
2024-05-14 23:32:18 +0100

    Word to outline component story

---
+++ website/src/stories/WordToOutline.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+import WordToOutline from '$lib/WordToOutline.svelte';
+
+const meta: Meta<typeof WordToOutline> = {
+	component: WordToOutline,
+	title: 'Disemvowelment/Word to Outline',
+	tags: ['autodocs'],
+	argTypes: {
+		word: {
+			control: 'text',
+			description: 'The word to convert into an outline.'
+		}
+	}
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+	args: {
+		word: 'bellow'
+	}
+};
