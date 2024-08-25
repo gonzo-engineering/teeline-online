7aab7171a3eb90e95dac860fccfe5a3b25ccb872
Frederick O'Brien <frederik.obrien@gmail.com>
2024-08-25 15:50:00 +0100

    Add CSS reset
    https://www.joshwcomeau.com/css/custom-css-reset/

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '$lib/styles/reset.css';
 	import '$lib/styles/global.css';
 	import Header from '../lib/layout/Header.svelte';
 	import Footer from '../lib/layout/Footer.svelte';
