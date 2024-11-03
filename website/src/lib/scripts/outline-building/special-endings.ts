a701541307b7a71d5c1d4301ba276c52aaa3fca5
Frederick O'Brien <frederik.obrien@gmail.com>
2024-11-03 23:19:47 +0000

    Flick in different direction depending on preceding letter

---
+++ website/src/lib/scripts/outline-building/special-endings.ts
@@ -0,0 +1,20 @@
+import type { OutlineObject } from '$lib/data/interfaces/interfaces';
+
+export const downUpI: OutlineObject = {
+	letterGroupings: ['i'],
+	specialOutlineMeanings: [],
+	lines: [
+		{
+			path: 'M324 459.7848c10.2791 -7.8446 21.7294 -20.9023 30.649 -30.8644c2.9172 -3.258 6.2931 -6.5014 8.8771 -10.0291c1.063 -1.4513 6.9035 -6.6916 6.1955 -5.7871',
+			length: 132,
+			start: {
+				x: 324,
+				y: 459.7848
+			},
+			end: {
+				x: 324,
+				y: 459.7848
+			}
+		}
+	]
+};
