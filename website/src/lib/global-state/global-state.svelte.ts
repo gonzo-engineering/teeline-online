faf97e5e101d2f781a96f915d2ddcf455b5b5768
Jaydip Sanghani <91427591+jellydeck@users.noreply.github.com>
2025-12-04 10:38:03 +0530

    finalized relevant changes

--- website/src/lib/global-state/global-state.svelte.ts
+++ website/src/lib/global-state/global-state.svelte.ts
@@ -1,23 +1,21 @@
 export const fontFamilies = ['Handwriting', 'No Tears', 'Indie Flower'];
-export const themes = ['light', 'dark', 'banana'];
+export const themes = ['light', 'dark'];
 
-export const user: { wpm: number; fontFamily: string; theme: 'light' | 'dark' | 'banana' } = $state(
-	{
-		wpm: 60,
-		fontFamily: 'Handwriting',
-		theme: 'light'
-	}
-);
+export const user: { wpm: number; fontFamily: string; theme: 'light' | 'dark' } = $state({
+	wpm: 60,
+	fontFamily: 'Handwriting',
+	theme: 'light'
+});
 
 export function setFontFamily(option: string) {
 	user.fontFamily = option;
-	document.documentElement.style.setProperty('--font-family', option + ', Indie Flower, cursive');
+	document.documentElement.style.setProperty('--font-family', option + ', cursive');
 }
 
 export function toggleTheme() {
 	const currentIndex = themes.indexOf(user.theme);
 	const nextIndex = (currentIndex + 1) % themes.length;
-	user.theme = themes[nextIndex] as 'light' | 'dark' | 'banana';
+	user.theme = themes[nextIndex] as 'light' | 'dark';
 
 	document.documentElement.className = user.theme;
 }
